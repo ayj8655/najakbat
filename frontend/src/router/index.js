da394c9da4b42059e0da9ce76027c76d80d04be2
semipumpkin <hohemian@gmail.com>
2021-07-26 16:45:40 +0900

    Sidebar Router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,11 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
+import MyAlerts from "../views/Sidebar/MyAlerts.vue"
+import DirectQuery from "../views/Sidebar/DirectQuery.vue"
+import Settings from "../views/Sidebar/Settings.vue"
+import SidebarMain from "../views/Sidebar/SidebarMain.vue"
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -18,6 +21,27 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  // 준호 라우터
+  {
+    path: '/MyAlerts',
+    name: 'MyAlerts',
+    component: MyAlerts,
+  },
+  {
+    path: '/directquery',
+    name: 'DirectQuery',
+    component: DirectQuery,
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: Settings,
+  },
+  {
+    path: '/sidebarmain',
+    name: 'SidebarMain',
+    component: SidebarMain,
   },
 ];
 
---
+++ frontend/src/views/Sidebar/MyAlerts.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>MyAlerts</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
