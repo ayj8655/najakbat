dada9c19f91cb37beb6c1810286919c4507b00c9
이용직 <yj_7679@naver.com>
2021-07-28 10:46:06 +0900

    커뮤니티 코드 작성

---
+++ backend/src/main/java/com/mococo/common/model/Post.java
@@ -0,0 +1,31 @@
+package com.mococo.common.model;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) 
+@Entity(name="post")
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int postNumber;
+	
+	private int userNumber;
+	private int postType; // 1:자유, 2:정보, 3:질문, 4:나눔
+	private String title;
+	private int recommend;
+	private int view;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date;
+	private String content;
+	private String keyword;
+	private int isdeleted; // 게시글이 지워졌는지 여부 - 지워졌어도 db에는 존재
+	
+	
+}
