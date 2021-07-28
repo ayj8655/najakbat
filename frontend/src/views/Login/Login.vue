588ce363e323148d641260fc6fad3ecf2d1611f2
multicampus <rlgktls@naver.com>
2021-07-28 21:33:20 +0900

    아이디 찾기/비밀번호 찾기 페이지 구현

--- frontend/src/views/Login/Login.vue
+++ frontend/src/views/Login/Login.vue
@@ -4,15 +4,18 @@
       <h2 class="mb-5">로그인</h2>
       <form action="">
         <div class="mb-3 container w-75">
-          <label for="" class="form-label d-flex align-items-start">아이디</label>
-          <input type="text" class="form-control mb-3" id="" placeholder="아이디를 입력하세요">
-          <label for="" class="form-label d-flex align-items-start">비밀번호</label>
-          <input type="password" class="form-control mb-3" id="" placeholder="비밀번호를 입력하세요">
+          <label for="username" class="form-label d-flex align-items-start">아이디</label>
+          <input type="text" class="form-control mb-3" id="username" placeholder="아이디를 입력하세요" v-model="credentials.username">
+          <label for="password" class="form-label d-flex align-items-start">비밀번호</label>
+          <input type="password" class="form-control mb-3" id="password" placeholder="비밀번호를 입력하세요" v-model="credentials.password">
           <div class="d-flex flex-row-reverse">
             <router-link to="/signup">회원가입</router-link>
+            <router-link to="/findid">아이디 찾기</router-link>
+            <router-link to="/findpassword">비밀번호 찾기</router-link>
           </div>
         </div>
-        <button class="btn btn-success">로그인 하기</button>
+        <button class="btn btn-secondary" v-if="!credentials.username || !credentials.password">로그인 하기</button>
+        <button class="btn btn-success" v-else>로그인 하기</button>
       </form>
     </div>
   </div>
@@ -20,6 +23,15 @@
 
 <script>
 export default {
+  name: 'Login',
+  data() {
+    return {
+      credentials: {
+        username: null,
+        password: null
+      }
+    }
+  }
 
 }
 </script>
