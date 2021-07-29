86d7ff9ac6d6743a2c352af5790fa6f3cad51d8e
semipumpkin <hohemian@gmail.com>
2021-07-29 15:17:56 +0900

    settings update

--- frontend/src/views/Sidebar/MyAlerts.vue
+++ frontend/src/views/Sidebar/MyAlerts.vue
@@ -1,10 +1,35 @@
 <template>
+<div>
+
   <div>MyAlerts</div>
+  <div v-for="notice in notices" :key="notice.noticeNumber">
+    <div>
+      {{notice.content}}
+    </div>
+    <div>
+      {{notice.date}}
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
+// import { mapActions } from 'vuex'
 export default {
-
+  name: 'Myalerts',
+  data () {
+    return {
+      notices: this.$store.state.searchNotices
+    }
+  },
+  computed: {
+    mynotices() {
+      return this.$store.state.searchNotices
+    }
+  },
+  created() {
+    this.$store.dispatch('getSearchNotice')
+  }
 }
 </script>
 
--- frontend/src/views/Sidebar/Settings.vue
+++ frontend/src/views/Sidebar/Settings.vue
@@ -8,46 +8,48 @@
     <div class="p-2 d-flex bd-highlight">
     <!-- <toggle-button @change="onChangeEventHandler"/> -->
     <h2>물 주기 알림</h2>
-    <toggle-button :value="mywater_alerts"
+    <toggle-button :value="mywater_notice"
                color="#82C7EB"
                :sync="true"
                :labels="true"
-               @change="changeWaterAlerts ()"/>
+               @change="changeWaterNotice ()"/>
     </div>
     <div class="p-2 d-flex bd-highlight">
     <h2>추천 정보 알림</h2>
     <!-- <toggle-button v-model="myDataVariable"/> -->
-    <toggle-button :value="myrecommendedinfo_alerts"
+    <toggle-button :value="myrecommendedinfo_notice"
                color="#82C7EB"
                :sync="true"
                :labels="true"
-               @change="changeRecommendedinfoAlerts ()"/>  
+               @change="changeRecommendedinfoNotice ()"/>  
     </div>
     <div class="p-2 d-flex bd-highlight">
     <h3>커뮤니티 새 댓글 알림</h3>
-    <toggle-button :value="mynewcomments_alerts"
+    <toggle-button :value="mynewcomments_notice"
                color="#82C7EB"
                :sync="true"
                :labels="true"
-               @change="changeNewcommentsAlerts ()"/>
+               @change="changeNewcommentsNotice ()"/>
     </div>
     <div class="p-2 d-flex bd-highlight">
     <h2>새 쪽지 알림</h2>
-    <toggle-button :value="mynewtwits_alerts"
+    <toggle-button :value="mynewtwits_notice"
                color="#82C7EB"
                :sync="true"
                :labels="true"
-               @change="changeNewtwitsAlerts ()"/>  
+               @change="changeNewtwitsNotice ()"/>  
     </div>
     <div class="p-2 d-flex bd-highlight">
     <h2>나이트모드</h2>
-    <toggle-button :value="mynightmode_alerts"
+    <toggle-button :value="mynightmode_notice"
                color="#82C7EB"
                :sync="true"
                :labels="true"
                @change="changeNightmode ()"/>
     
     </div>
+    <button type="button" class="btn btn-success mb-2" v-if="changes">저장</button>
+    <button type="button" class="btn btn-success" v-else disabled>저장</button>
   </div>  
 
 
@@ -63,31 +65,76 @@ export default {
   name: 'AlertsSettings',
   data() {
     return {
-      mywater_alerts: this.$store.state.water_alerts,
-      myrecommendedinfo_alerts: this.$store.state.recommendedinfo_alerts,
-      mynewcomments_alerts: this.$store.state.newcomments_alerts,
-      mynewtwits_alerts: this.$store.state.newtwits_alerts,
-      mynightmode_alerts: this.$store.state.nightmode_alerts,
+      mywater_notice: this.$store.state.water_notice,
+      myrecommendedinfo_notice: this.$store.state.recommendedinfo_notice,
+      mynewcomments_notice: this.$store.state.newcomments_notice,
+      mynewtwits_notice: this.$store.state.newtwits_notice,
+      mynightmode_notice: this.$store.state.nightmode_notice,
+      changes: false,
     }
   },
   methods: {
-    changeWaterAlerts () {
-      this.mywater_alerts = !this.mywater_alerts
+    changeWaterNotice () {
+      this.mywater_notice = !this.mywater_notice
     },
-    changeRecommendedinfoAlerts () {
-      this.myrecommendedinfo_alerts = !this.myrecommendedinfo_alerts
+    changeRecommendedinfoNotice () {
+      this.myrecommendedinfo_notice = !this.myrecommendedinfo_notice
     },
-    changeNewcommentsAlerts () {
-      this.mynewcomments_alerts = !this.mynewcomments_alerts
+    changeNewcommentsNotice () {
+      this.mynewcomments_notice = !this.mynewcomments_notice
     },
-    changeNewtwitsAlerts () {
-      this.mynewtwits_alerts = !this.mynewtwits_alerts
+    changeNewtwitsNotice () {
+      this.mynewtwits_notice = !this.mynewtwits_notice
     },
     changeNightmode () {
-      this.mynightmode_alerts = !this.mynightmode_alerts
+      this.mynightmode_notice = !this.mynightmode_notice
+    },
+    settingsUpdate() {
+      this.$store.dispatch('updateNotcie', this.mywater_notice, this.myrecommendedinfo_notice, this.mynewcomments_notice, this.mynewtwits_notice, this.mynightmode_notice)
+    }
+  
+  },
+  watch: {
+    mywater_notice: function () {
+      if (this.changes) {
+        this.changes = true
+      } else {
+        this.changes = ! this.changes
+      }
+    },
+    myrecommendedinfo_notice: function () {
+      if (this.changes) {
+        this.changes = true
+      } else {
+        this.changes = ! this.changes
+      }
+    },
+    mynewcomments_notice: function () {
+      
+      if (this.changes) {
+        this.changes = true
+      } else {
+        this.changes = ! this.changes
+      }
+    },
+    mynewtwits_notice: function () {
+      if (this.changes) {
+        this.changes = true
+      } else {
+        this.changes = ! this.changes
+      }
+      
+    },
+    mynightmode_notice: function () {
+      if (this.changes) {
+        this.changes = true
+      } else {
+        this.changes = ! this.changes
+      }
+      
     },
 
-  },
+  }
   // computed: {
   //   ...mapState([
   //     'water_alerts',
