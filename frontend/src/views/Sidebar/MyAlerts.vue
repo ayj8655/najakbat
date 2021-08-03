dea674a7c3752ec29f0e12e58ce0f72bd2b7ed76
semipumpkin <hohemian@gmail.com>
2021-08-04 00:17:11 +0900

    myalerts delete

--- frontend/src/views/Sidebar/MyAlerts.vue
+++ frontend/src/views/Sidebar/MyAlerts.vue
@@ -2,17 +2,19 @@
 <div>
   <header-nav></header-nav>
   <router-view></router-view>
-  <h3 class="mb-5 mt-5 fw-bold">알림</h3>
+  <h3 class="mb-3 mt-5 fw-bold">알림</h3>
+  <button type="button" class="btn btn-danger mb-2" @click="deleteallNotices">Danger</button>
   <div v-for="searchNotice in searchNotices" :key="searchNotice.noticeNumber">
-    <div class="container px-4">
-      <div class="isRead-false border border-end-0 border-start-0" @click="Reading(searchNotice)">
+    <div class="container px-0">
+      <div v-if="searchNotice.isRead" class="isRead-false border border-end-0 border-start-0 bg-white" draggable @click="Reading(searchNotice)">
         <div class="container notice mt-2">
           <div class="row">
+            <div class="col-1 px-0" @drop="deleteNotice" @dragover.prevent @dragenter.prevent></div>
             <div class="col-2 px-0">
               <img v-if="searchNotice.isRead" src="@/assets/noti.png" width="30px" height="30px" alt="">
-              <img v-else src="@/assets/noti_green.png" width="30px" height="30px" alt="">
+              <!-- <img v-else src="@/assets/noti_green.png" width="30px" height="30px" alt=""> -->
             </div>
-            <div class="col-10 px-0">
+            <div class="col-8 px-0">
               <div class="content">
                 {{searchNotice.content}}
               </div>
@@ -20,6 +22,27 @@
               {{ searchNotice.date | moment('YYYY-MM-DD HH:mm:ss') }} 
               </div>
             </div>
+            <div class="col-1 px-0"></div>
+          </div>
+        </div>
+      </div>
+      <div v-else class="isRead-false border border-end-0 border-start-0" draggable @click="Reading(searchNotice)">
+        <div class="container notice mt-2">
+          <div class="row">
+            <div class="col-1 px-0" @drop="deleteNotice" @dragover.prevent @dragenter.prevent></div>
+            <div class="col-2 px-0">
+              <!-- <img v-if="searchNotice.isRead" src="@/assets/noti.png" width="30px" height="30px" alt=""> -->
+              <img src="@/assets/noti_green.png" width="30px" height="30px" alt="">
+            </div>
+            <div class="col-8 px-0">
+              <div class="content">
+                {{searchNotice.content}}
+              </div>
+              <div class="date mx-1">
+              {{ searchNotice.date | moment('YYYY-MM-DD HH:mm:ss') }} 
+              </div>
+            </div>
+            <div class="col-1 px-0"></div>
           </div>
         </div>
       </div>
@@ -47,6 +70,8 @@ export default {
   methods: {
     ...mapActions ([
       'updateIsread',
+      'deleteallNotices',
+      'deleteNotice',
     ]),
     Reading (mynotice) {
       mynotice.isRead = 1
@@ -54,9 +79,11 @@ export default {
       const mynoticeStatus = [mynotice.isRead, mynotice.noticeNumber]
       // console.log(this.$store.state.searchNotices)
       this.$store.state.searchNotices[mynoticeStatus[1] - 1].isRead = mynoticeStatus[0] 
-
       this.$store.dispatch('updateIsread', mynoticeStatus)
     },
+    test () {
+      console.log('success')
+    }
   },
   computed: {
     ...mapState([
