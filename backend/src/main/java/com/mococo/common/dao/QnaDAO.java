032544ef0c224cf29e8bbcffda253fac9d26bf19
이용직 <yj_7679@naver.com>
2021-08-09 22:38:16 +0900

    qna 기능구현완료

---
+++ backend/src/main/java/com/mococo/common/dao/QnaDAO.java
@@ -0,0 +1,28 @@
+package com.mococo.common.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.mococo.common.model.Qna;
+
+@Repository
+public interface QnaDAO extends JpaRepository<Qna, Integer>{
+
+
+
+	Optional<Qna> findQnaByQnaNumber(int no);
+	
+	// limit은 어디부터 3개씩 가져올지 선택 no는 유저 번호
+	@Query(value = "SELECT q "
+					+ "FROM qna q "
+					+ "WHERE user_number = :no ")	
+	List<Object> findAllByUserNumber(int no, Pageable pageable);
+
+
+
+}
---
+++ backend/src/main/java/com/mococo/common/service/QnaService.java
@@ -0,0 +1,62 @@
+package com.mococo.common.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.mococo.common.dao.QnaDAO;
+import com.mococo.common.model.Qna;
+
+
+
+@Service
+public class QnaService {
+	@Autowired
+	QnaDAO qnaDAO;
+
+	public Qna insertQna(Qna qna) {
+		Optional<Qna> ret = qnaDAO.findQnaByQnaNumber(qna.getQnaNumber());
+		if(ret.isPresent()) {
+			return null;
+		}
+		
+		
+		Qna p = qnaDAO.save(qna);
+		return p;
+	}
+
+	public boolean deletePost(int no) {
+		Optional<Qna> ret = qnaDAO.findQnaByQnaNumber(no);
+		
+		// delete할 post가 없는 경우
+		if(!ret.isPresent()) {
+			return false;
+		}
+		
+		qnaDAO.deleteById(no);
+		return true;
+	}
+
+	public boolean answerQna(String qnano, String answer) {
+		Optional<Qna> ret = qnaDAO.findQnaByQnaNumber(Integer.parseInt(qnano));
+		
+		// update할 post가 없는 경우
+		if(!ret.isPresent()) {
+			return false;
+		}
+		ret.get().setAnswer(answer);
+		ret.get().setFinish(true);
+		
+		qnaDAO.save(ret.get());
+		return true;
+	}
+
+	public List<Object> findQnaUser(int no, int limit) {
+		List<Object> qnas = qnaDAO.findAllByUserNumber(no,PageRequest.of(limit, 3,Sort.by("date").descending()));
+		return qnas;
+	}
+}
