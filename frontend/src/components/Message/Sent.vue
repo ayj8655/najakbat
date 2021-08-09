4b4860c6817f00ec0e04381630a02db3821eaae4
semipumpkin <hohemian@gmail.com>
2021-08-09 09:23:01 +0900

    feature/message

--- frontend/src/components/Message/Sent.vue
+++ frontend/src/components/Message/Sent.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>{{ sentMessages }}
-
+  <div class="container">
+    <div v-for="sentMessage in sentMessages" :key="sentMessage.noticeNumber">
+    <div class="container px-0">
+      <div class="isRead-false border border-end-0 border-start-0 bg-white">
+        <div class="container notice mt-2">
+          <div class="row">
+            <div class="col-1 px-0">
+              
+            </div>
+            <div class="col-2 px-0">
+             
+            </div>
+            <div class="col-8 px-0">
+              <div class="content">
+                {{sentMessage.content}}
+              </div>
+              <div class="date mx-1">
+                {{ sentMessage.time | moment('YYYY-MM-DD HH:mm:ss') }} 
+              </div>
+            </div>
+            <div class="col-1 px-0"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
