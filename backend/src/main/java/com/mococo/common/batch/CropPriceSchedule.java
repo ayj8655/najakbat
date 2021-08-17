cf543919a16e6b31cb73b7cec84d2f8eac4c305e
ayj8655 <ayj8655@naver.com>
2021-08-17 14:43:41 +0900

    닉네임으로번호찾기+배치 설정

--- backend/src/main/java/com/mococo/common/batch/CropPriceSchedule.java
+++ backend/src/main/java/com/mococo/common/batch/CropPriceSchedule.java
@@ -17,17 +17,17 @@ public class CropPriceSchedule {
 	@Autowired
 	CropService cropService;
 
-//	@Scheduled(cron = "0 0 22 * * *") // 저녁 22시에 실행
-//	public void task() throws Exception {
-//		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-//		Date date = new Date();
-//		Calendar startCalendar = Calendar.getInstance();
-//		startCalendar.add(Calendar.DATE, -1);
-//		
-//		String start = format.format(startCalendar.getTime());
-//		String end = format.format(date);
-//		System.out.println("crop price data insert : " + start + "~" + end + " : " + LocalDateTime.now());
-//		cropService.insertCropPrice(start, end);
-//		System.out.println("crop price data insert finish : " + LocalDateTime.now());
-//	}
+	@Scheduled(cron = "0 0 22 * * *") // 저녁 22시에 실행
+	public void task() throws Exception {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		Calendar startCalendar = Calendar.getInstance();
+		startCalendar.add(Calendar.DATE, -1);
+		
+		String start = format.format(startCalendar.getTime());
+		String end = format.format(date);
+		System.out.println("crop price data insert : " + start + "~" + end + " : " + LocalDateTime.now());
+		cropService.insertCropPrice(start, end);
+		System.out.println("crop price data insert finish : " + LocalDateTime.now());
+	}
 }
